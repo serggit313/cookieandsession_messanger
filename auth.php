27d9e03fa9f3cc2f2cac67df3674dad381683918
Sergei <sergei.militscky@yandex.ru>
2020-11-30 17:43:20 +0300

    1

---
+++ auth.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Регистрация</title>
+</head>
+<body>
+<div class="form_register">
+        <h3>Авторизация: </h3>
+        <form action="#" method="POST">
+            <p>
+                <input type="text" name="login" placeholder="Login">
+            </p>
+            <p>
+                <input type="text" name="password" placeholder="Password">
+            </p>
+            <p>
+                <input type="checkbox" name="check">
+            </p>
+            <button type="submit">Отправить</button>
+        </form>
+    </div>
+    <br>
+    <a href="index.php">На главную</a>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,5 +1,89 @@
 <?php
+session_start();
+// Функция валидации
+function validate()
+{
+    if(!empty($_POST['name']) && !empty($_POST['comment']))
+    {
+        saveComment('Database.txt', $_POST['name'], $_POST['comment']);
+    }
+}
 
-echo "Привет,Мир!";
+validate();
+
+function saveComment($document, $name, $comment)
+{
+    if(file_exists($document))
+    {
+        $w = $name . ': '. $comment . "\n"; 
+        file_put_contents($document, $w, FILE_APPEND);
+    }
+}
+
+function showComment($document)
+{
+    if(file_exists($document))
+    {
+       $data_arr = file($document);
+       $revers = array_reverse($data_arr);
+       foreach($revers as $key=>$value)
+       {?>
+        <p>
+            <span>
+                <?php echo $value;?>
+            </span>
+        </p>
+       <?php }
+    }
+}
+
+// Проверяем пользователя гость ли он или уже открыта сессия
+function is_guest()
+{
+    if(!empty($_SESSION['user'] || !empty($_COOKIE['remember'])))
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
 
 ?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Главная</title>
+</head>
+<body>
+    <?php if(is_guest() == true):?>
+    <h1>Только авторезированные пользователи могут оставлять комментарии</h1><br/>
+    <div class="reg_links">
+        <a href="auth.php" style="margin-right: 30px;">Авторизация</a> 
+        <a href="reg.php">Регистрация</a>
+    </div>
+    <?php else:?>
+    <form action="" method="POST">
+        <div>
+            <input type="text" placeholder="Имя" name="name">
+        </div>
+        <div>
+            <textarea name="comment" id="comment" cols="30" rows="10" placeholder="Комментарий: "></textarea>
+        </div>
+        <button type="submit">Отправить</button>
+    </form>
+
+    <div class="comments">
+        <?php showComment('Database.txt'); ?>
+    </div>
+
+    <div class="exit">
+        <a href="#">Выйти</a>
+    </div>
+    <?php endif;?>
+
+</body>
+</html>
